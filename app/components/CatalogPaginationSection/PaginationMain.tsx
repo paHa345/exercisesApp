fc3cc98e9ac8d225ca9dd998de12892dd2669c43
paHa345 <pav.345@mail.ru>
2024-05-13 16:35:30 +0700

    v 0.947 fix exercises pagination mechanism

--- app/components/CatalogPaginationSection/PaginationMain.tsx
+++ app/components/CatalogPaginationSection/PaginationMain.tsx
@@ -8,6 +8,8 @@ import {
 import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faXmark, faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 const PaginationMain = () => {
   const searchParams = useSearchParams();
@@ -21,7 +23,8 @@ const PaginationMain = () => {
 
   const changePageButtonHandler = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
-    const numberPageButton = Number(e.currentTarget.textContent);
+    const numberPageButton = Number(e.currentTarget.dataset.gotopage);
+
     if (numberPageButton !== currentPage) {
       //изменить url
       // изменить currentExercisesPage
@@ -48,19 +51,55 @@ const PaginationMain = () => {
     }
   };
 
+  //   const addButton = ()=> {
+
+  //   }
+
   const pagesCount = Math.ceil(exercisesCount / 3);
+  //   const pagesCount = Math.ceil(exercisesCount);
   const pagesButton = [];
-  for (let i = 1; i <= pagesCount; i++) {
-    pagesButton.push(
-      <button
-        onClick={changePageButtonHandler}
-        className={`${currentPage === i ? `bg-gray-600 text-slate-100` : `bg-stone-200`}  shadow-cardElementStartShadow self-center mx-3 my-3 py-1 px-2 rounded-md  hover:bg-gray-600  hover:text-slate-100  hover:shadow-cardElementShadow `}
-        key={i}
-      >
-        {i}
-      </button>
-    );
+  let buttonCounter = 7;
+  for (let i = currentPage - 3; i <= pagesCount; i++) {
+    if (i === currentPage - 3) {
+      pagesButton.push(
+        <button
+          onClick={changePageButtonHandler}
+          className={`${currentPage === 1 ? `bg-gray-600 text-slate-100` : `bg-stone-200`}  shadow-cardElementStartShadow self-center mx-3 my-3 py-1 px-2 rounded-md  hover:bg-gray-600  hover:text-slate-100  hover:shadow-cardElementShadow `}
+          key={"GoToFirst"}
+          data-gotopage={1}
+        >
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </button>
+      );
+    }
+    if (i > 0 && buttonCounter > 0) {
+      pagesButton.push(
+        <button
+          onClick={changePageButtonHandler}
+          className={`${currentPage === i ? `bg-gray-600 text-slate-100` : `bg-stone-200`}  shadow-cardElementStartShadow self-center mx-3 my-3 py-1 px-2 rounded-md  hover:bg-gray-600  hover:text-slate-100  hover:shadow-cardElementShadow `}
+          key={i}
+          data-gotopage={i}
+        >
+          {i}
+        </button>
+      );
+      buttonCounter--;
+    }
+
+    if (i === pagesCount) {
+      pagesButton.push(
+        <button
+          onClick={changePageButtonHandler}
+          className={`${currentPage === pagesCount ? `bg-gray-600 text-slate-100` : `bg-stone-200`}  shadow-cardElementStartShadow self-center mx-3 my-3 py-1 px-2 rounded-md  hover:bg-gray-600  hover:text-slate-100  hover:shadow-cardElementShadow `}
+          key={"GoToLast"}
+          data-gotopage={pagesCount}
+        >
+          <FontAwesomeIcon icon={faChevronRight} />
+        </button>
+      );
+    }
   }
+
   return (
     <div>
       {pagesCount > 1 && (
