ba17de9a07bafdc3a728b9adc24cb2586b0f32c2
paHa345 <pav.345@mail.ru>
2024-10-18 14:42:15 +0700

    v 0.9993 add server action in api v3

--- app/api/revalidate/route.tsx
+++ app/api/revalidate/route.tsx
@@ -4,6 +4,7 @@ import { NextRequest } from "next/server";
 export async function GET(request: NextRequest) {
   try {
     const path = request.nextUrl.searchParams.get("path") || "/";
+    console.log(path);
     revalidatePath(path);
     return Response.json({ revalidated: true, now: Date.now() });
   } catch (error) {
--- app/catalog/editExercise/page.tsx
+++ app/catalog/editExercise/page.tsx
@@ -1,9 +1,19 @@
 import EditExerciseMain from "@/app/components/EditExerciseSection/EditExerciseMain";
 import { IAppSlice } from "@/app/store/appStateSlice";
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const editExercise = () => {
+  const testFunc = async function () {
+    const revalidate = await fetch("./../api/revalidate", { next: { revalidate: 1 } });
+    const revRes = await revalidate.json();
+    console.log(revRes);
+  };
+
+  useEffect(() => {
+    testFunc();
+  }, []);
+
   return <EditExerciseMain></EditExerciseMain>;
 };
 
