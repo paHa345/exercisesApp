ae28ca3c4e75065346b5f9ac7e6f0e5a6e01f694
paHa345 <pav.345@mail.ru>
2024-10-18 09:48:09 +0700

    v 0.9993 add server action in api v3

--- app/components/EditExerciseSection/EditExerciseCard.tsx
+++ app/components/EditExerciseSection/EditExerciseCard.tsx
@@ -121,7 +121,7 @@ const EditExerciseCard = () => {
     // const revalidate = await fetch("./../api/revalidate", { next: { revalidate: 1 } });
     // revalidatePath(`https://exercises-app-one.vercel.app/catalog/66582fb1b825f8f115bc936e`);
 
-    editExerciseRevalidateServerAction(editedExercise?._id);
+    // editExerciseRevalidateServerAction(editedExercise?._id);
   };
 
   const mainMuscleGroupElement = mainMuscleGrourArr.map((mainMuscleGroup) => {
