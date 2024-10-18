6302f339f13f5dcddd2c0474bd1435276e5a3c00
paHa345 <pav.345@mail.ru>
2024-10-18 12:21:18 +0700

    v 0.9993 add server action in api v3

--- actions/editExercise.tsx
+++ actions/editExercise.tsx
@@ -4,5 +4,5 @@ import { revalidatePath } from "next/cache";
 
 export const editExerciseRevalidateServerAction = async (id: string | undefined | string[]) => {
   console.log(`/catalog/${id}`);
-  revalidatePath(`/catalog/${id}`);
+  revalidatePath(`/catalog/66582fb1b825f8f115bc936e`);
 };
