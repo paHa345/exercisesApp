03ac3f2fc5fca8695141f8ce83d486bc860b18e0
paHa345 <pav.345@mail.ru>
2024-10-18 11:40:39 +0700

    v 0.9993 add server action in api v3

--- actions/editExercise.tsx
+++ actions/editExercise.tsx
@@ -3,5 +3,6 @@
 import { revalidatePath } from "next/cache";
 
 export const editExerciseRevalidateServerAction = async (id: string | undefined | string[]) => {
-  revalidatePath(`https://exercises-app-one.vercel.app/catalog/66582fb1b825f8f115bc936e`);
+  console.log(`/catalog/${id}`);
+  revalidatePath(`/catalog/${id}`);
 };
