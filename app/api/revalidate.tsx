88bc6ae7e2ad1797c40c5586cac64adb0d17fedb
paHa345 <pav.345@mail.ru>
2024-10-21 15:44:45 +0700

    v 0.9993 add server action in api v3

--- app/api/revalidate.tsx
+++ app/api/revalidate.tsx
@@ -1,18 +1,31 @@
-import { revalidatePath } from "next/cache";
-import { NextRequest, NextResponse } from "next/server";
+// import { revalidatePath } from "next/cache";
+// import { NextRequest, NextResponse } from "next/server";
 
-export default async function GET(req: NextRequest, res: NextResponse) {
-  // Check for secret to confirm this is a valid request
-  // if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
-  //   return res.status(401).json({ message: 'Invalid token' })
-  // }
+// export default async function GET(req: NextRequest, res: NextResponse) {
+//   // Check for secret to confirm this is a valid request
+//   // if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+//   //   return res.status(401).json({ message: 'Invalid token' })
+//   // }
 
+//   try {
+//     revalidatePath("/", "layout");
+//     return Response.json({ revalidated: true, now: Date.now() });
+//   } catch (error: any) {
+//     // If there was an error, Next.js will continue
+//     // to show the last successfully generated page
+//     return NextResponse.json({ message: error?.message }, { status: 400 });
+//   }
+// }
+
+import { NextApiHandler } from "next";
+
+const handler: NextApiHandler = async (req, res) => {
   try {
-    revalidatePath("/", "layout");
-    return Response.json({ revalidated: true, now: Date.now() });
-  } catch (error: any) {
-    // If there was an error, Next.js will continue
-    // to show the last successfully generated page
-    return NextResponse.json({ message: error?.message }, { status: 400 });
+    await res.revalidate("/catalog/test");
+    return res.json({ revalidated: true });
+  } catch (err) {
+    return res.status(500).send("Error revalidating");
   }
-}
+};
+
+export default handler;
