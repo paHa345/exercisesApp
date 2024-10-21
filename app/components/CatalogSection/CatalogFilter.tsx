32c54f21663db5847a3b2cce90d830264f556fbb
paHa345 <pav.345@mail.ru>
2024-10-21 15:58:33 +0700

    v 0.9993 add server action in api v3

--- app/components/CatalogSection/CatalogFilter.tsx
+++ app/components/CatalogSection/CatalogFilter.tsx
@@ -33,7 +33,7 @@ const CatalogFilter = () => {
   return (
     <div className=" flex justify-center py-5">
       <select
-        defaultValue={String(searchParams.get("filter"))}
+        defaultValue={String(searchParams?.get("filter"))}
         onChange={changeCatalogFilterHandler}
         name="catalogSelect"
         id="catalogSelect"
