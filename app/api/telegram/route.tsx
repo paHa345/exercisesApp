127feaed9fd3d466a22dca1336196782c5fc1106
paHa345 <pav.345@mail.ru>
2024-11-18 11:08:32 +0700

    v 0.99974 add telegram bot

--- app/api/telegram/route.tsx
+++ app/api/telegram/route.tsx
@@ -6,7 +6,7 @@ export async function POST(req: any, res: NextResponse) {
   //   if (req.body.message === "/start") {
   const message = `Welcome to crossword Game ${req.body.message}`;
   const ret = await fetch(
-    `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=-1007577331969&text=${message}&parse_mode=HTML`
+    `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=-100991389784&text=${message}&parse_mode=HTML`
   );
   //   }
   return NextResponse.json({ message: "Success", result: "OK!" });
