b740e89704e2ea72b9bf7a14ba556e115cff0fef
paHa345 <pav.345@mail.ru>
2024-11-18 10:15:28 +0700

    v 0.99974 add telegram bot

--- app/api/telegram/route.tsx
+++ app/api/telegram/route.tsx
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: any, res: NextResponse) {
   const tgbot = process.env.NEXT_TELEGRAM_TOKEN;
-  if (req.body.message.text === "/start") {
+  if (req.body.message === "/start") {
     const message = `Welcome to crossword Game ${req.body.message}`;
     const ret = await fetch(
       `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat_id}&text=${message}&parse_mode=HTML`
