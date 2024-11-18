2bb05e0775cf8ecb514cda22d65e09fd12b6baa4
paHa345 <pav.345@mail.ru>
2024-11-18 09:46:46 +0700

    v 0.99974 add telegram bot

--- app/api/telegram/route.tsx
+++ app/api/telegram/route.tsx
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export default async (req: NextRequest, res: NextResponse) => {
+export async function GET(req: NextRequest, res: NextResponse) {
   const tgbot = process.env.NEXT_TELEGRAM_TOKEN;
   console.log(req.body);
   return NextResponse.json({ message: "Success", result: "OK!" });
-};
+}
